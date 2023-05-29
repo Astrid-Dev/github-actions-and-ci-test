2293be571f3ccdb34391e4d541d078674f4cab08
Astrid-Dev <astrid.jounang.dev@gmail.com>
2023-05-29 18:24:02 +0100

    second attempt for the pull request

---
+++ build/calculator.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mathOperations = {
+    sum: function (a, b) {
+        return a + b;
+    },
+    diff: function (a, b) {
+        return a - b;
+    },
+    product: function (a, b) {
+        return a * b;
+    },
+    divide: function (a, b) {
+        return a / b;
+    }
+};
+exports.default = mathOperations;
+//# sourceMappingURL=calculator.js.map
